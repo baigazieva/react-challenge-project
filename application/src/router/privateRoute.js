1c4ed8eb0eed69fd171db611958e213fc4a335f3
saruwka <36380102+saruwka@users.noreply.github.com>
2021-03-16 01:40:56 -0700

    editing comment on a component

--- application/src/router/privateRoute.js
+++ application/src/router/privateRoute.js
@@ -4,6 +4,8 @@ import { Redirect, Route } from "react-router-dom";
 
 const PrivateRouter = ({ token, component: Component, ...rest }) => {
   return (
+      // Show the component only when the user is logged in
+        // Otherwise, redirect the user to /signin page
     <Route {...rest} render={
       (props) => token ? 
       <Component {...props}/> : 
